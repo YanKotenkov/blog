2132d4a0b83559b07ece03c486cbf32a33daf920
YanKotenkov <013shadow@gmail.com>
2016-10-07 21:48:09 +0500

    Blog

--- protected/config/main.php
+++ protected/config/main.php
@@ -20,14 +20,14 @@ return array(
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		/*
+
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>'defaultpasswd',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
-		*/
+
 	),
 
 	// application components
@@ -83,3 +83,19 @@ return array(
 		'adminEmail'=>'webmaster@example.com',
 	),
 );
+/*
+// Устанавливаем Gii
+return array(
+    'import'=>array(
+    'application.models.*',
+    'application.components.*',
+),
+
+    'modules'=>array(
+    'gii'=>array(
+        'class'=>'system.gii.GiiModule',
+        'password'=>'ваш пароль',
+    ),
+),
+);
+*/
