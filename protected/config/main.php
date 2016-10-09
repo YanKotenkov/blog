fa14e5d5acc24cf6dc06769aa3ca8a9c28503cec
YanKotenkov <013shadow@gmail.com>
2016-10-09 19:30:31 +0500

    Added some changes

--- protected/config/main.php
+++ protected/config/main.php
@@ -5,6 +5,7 @@
 
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
+/** @noinspection PhpDuplicateArrayKeysInspection */
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'My Web Application',
@@ -87,12 +88,12 @@ return array(
 		'adminEmail'=>'webmaster@example.com',
 	),
 
-    // Подключаем Gii
     'import'=>array(
         'application.models.*',
         'application.components.*',
     ),
 
+    // Подключаем Gii
     'modules'=>array(
         'gii'=>array(
             'class'=>'system.gii.GiiModule',
--- protected/models/Post.php
+++ protected/models/Post.php
@@ -19,6 +19,10 @@
  */
 class Post extends CActiveRecord
 {
+    const STATUS_DRAFT=1;
+    const STATUS_PUBLISHED=2;
+    const STATUS_ARCHIVED=3;
+
 	/**
 	 * @return string the associated database table name
 	 */
@@ -35,13 +39,15 @@ class Post extends CActiveRecord
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
-			array('title, content, status, author_id', 'required'),
-			array('status, create_time, update_time, author_id', 'numerical', 'integerOnly'=>true),
+			array('title, content, status', 'required'),
 			array('title', 'length', 'max'=>128),
-			array('tags', 'safe'),
+            array('status', 'in', 'range'=>array(1,2,3)),
+            array('tags', 'match', 'pattern'=>'/^[\w\s,]+$/',
+                'message'=>'В тегах можно использовать только буквы.'),
+            array('tags', 'normalizeTags'),
 			// The following rule is used by search().
 			// @todo Please remove those attributes that should not be searched.
-			array('id, title, content, tags, status, create_time, update_time, author_id', 'safe', 'on'=>'search'),
+            array('title, status', 'safe', 'on'=>'search'),
 		);
 	}
 
@@ -52,10 +58,14 @@ class Post extends CActiveRecord
 	{
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
-		return array(
-			'author' => array(self::BELONGS_TO, 'User', 'author_id'),
-			'comments' => array(self::HAS_MANY, 'Comment', 'post_id'),
-		);
+        return array(
+            'author' => array(self::BELONGS_TO, 'User', 'author_id'),
+            'comments' => array(self::HAS_MANY, 'Comment', 'post_id',
+                'condition'=>'comments.status='.Comment::STATUS_APPROVED,
+                'order'=>'comments.create_time DESC'),
+            'commentCount' => array(self::STAT, 'Comment', 'post_id',
+                'condition'=>'status='.Comment::STATUS_APPROVED),
+        );
 	}
 
 	/**
@@ -117,4 +127,44 @@ class Post extends CActiveRecord
 	{
 		return parent::model($className);
 	}
+
+
+    public function getUrl()
+    {
+        return Yii::app()->createUrl('post/view', array(
+            'id'=>$this->id,
+            'title'=>$this->title,
+        ));
+    }
+
+    protected function beforeSave()
+    {
+        if(parent::beforeSave())
+        {
+            if($this->isNewRecord)
+            {
+                $this->create_time=$this->update_time=time();
+                $this->author_id=Yii::app()->user->id;
+            }
+            else
+                $this->update_time=time();
+            return true;
+        }
+        else
+            return false;
+    }
+
+    protected function afterSave()
+    {
+        parent::afterSave();
+        Tag::model()->updateFrequency($this->_oldTags, $this->tags);
+    }
+
+    private $_oldTags;
+
+    protected function afterFind()
+    {
+        parent::afterFind();
+        $this->_oldTags=$this->tags;
+    }
 }
